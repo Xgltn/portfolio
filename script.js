745c2b41064a06b6418fb2507264ba1ca4e60e84
David <112301520+Xgltn@users.noreply.github.com>
2025-04-16 20:30:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add('visible');
+    }
+  }
+}
+
+window.addEventListener('scroll', revealOnScroll);
+
+// Pokreni odmah ako je nešto već vidljivo
+revealOnScroll();
 
