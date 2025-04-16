094ddc98748dce3eb5a7f2399ce92b9a09318c0e
David <112301520+Xgltn@users.noreply.github.com>
2025-04-16 19:51:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// Light mode toggle
 const toggleLight = document.getElementById('toggle-light');
 
 toggleLight.addEventListener('click', () => {
   document.body.classList.toggle('light-mode');
+
+  // opcionalno promijeni tekst gumba
+  if (document.body.classList.contains('light-mode')) {
+    toggleLight.textContent = "🌙 Dark Mode";
+  } else {
+    toggleLight.textContent = "☀️ Light Mode";
+  }
 });
- 
